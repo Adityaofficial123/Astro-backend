631f4aac4b95f9384d3b5e332cf29290a12226a2
Aditya's_Unofficial_Account <professionalwork149@gmail.com>
2026-03-21 17:30:02 +0530

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+    apiBaseUrl: "https://astro-backend-qg24.onrender.com",
+    firebase: {
+        apiKey: "AIzaSyC_1zweWRYkW9wrLTx1nSEE9hZHo0BCEkg",
+        authDomain: "astrosoc-72c8f.firebaseapp.com",
+        projectId: "astrosoc-72c8f",
+        databaseURL: "https://astrosoc-72c8f-default-rtdb.firebaseio.com",
+        storageBucket: "astrosoc-72c8f.firebasestorage.app",
+        messagingSenderId: "817813105793",
+        appId: "1:817813105793:web:f7d38982e7fb5d83f0b180"
+    }
+
+};
